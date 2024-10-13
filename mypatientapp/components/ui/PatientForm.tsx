c92373feb0370ac5f669a057c85293f674345546
itzkushagra <info.kushagrapandey@gmail.com>
2024-10-13 16:12:14 +0530

    setup appwrite for data storage and messaging

--- mypatientapp/components/ui/PatientForm.tsx
+++ mypatientapp/components/ui/PatientForm.tsx
@@ -3,11 +3,12 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomForm from "./ui/CustomForm"
 import SubmitButton from "./ui/SubmitButton"
 import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -21,25 +22,34 @@ export enum FormFieldType {
 
 
  
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
- 
+//const formSchema = z.object({
+  //username: z.string().min(2, {
+    //message: "Username must be at least 2 characters.",
+  //}),
+//})
+
 const PatientForm = () => {
+  const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
     
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-
-    console.log(values)
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      const user = {name, email, phone}
+      if(user) router.push('/patients/${user.$id}/register')
+    } catch (error) {
+      console.log(error);
+      
+    }
   }
     return(
         <Form {...form}>
